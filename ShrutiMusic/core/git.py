8291bc86ebd9948de63884ed192c4e4b24bde489
𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs <s>
2025-08-01 03:37:53 +0000

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/core/git.py
+++ ShrutiMusic/core/git.py
@@ -94,3 +94,9 @@ def git():
 
 
 # ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
+
+# ===========================================
+# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
+# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
+# 📢 Telegram Channel : https://t.me/ShrutiBots
+# ===========================================
