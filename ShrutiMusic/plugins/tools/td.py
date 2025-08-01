a5ebe785190ae4add897a1992c36fd8393e17f5d
𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs <s>
2025-08-01 03:37:07 +0000

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/plugins/tools/td.py
+++ ShrutiMusic/plugins/tools/td.py
@@ -83,3 +83,9 @@ __MODULE__ = "Tʀᴜᴛʜ"
 
 
 # ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
+
+# ===========================================
+# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
+# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
+# 📢 Telegram Channel : https://t.me/ShrutiBots
+# ===========================================
