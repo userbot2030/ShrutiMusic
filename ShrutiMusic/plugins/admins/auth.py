56a8e9e306c8983bead6a403045fec8192406ec3
𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs <s>
2025-08-01 03:17:40 +0000

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs - auth.py

--- ShrutiMusic/plugins/admins/auth.py
+++ ShrutiMusic/plugins/admins/auth.py
@@ -109,3 +109,6 @@ async def authusers(client, message: Message, _):
             text += f"{j}➤ {user}[<code>{user_id}</code>]\n"
             text += f"   {_['auth_8']} {admin_name}[<code>{admin_id}</code>]\n\n"
         await mystic.edit_text(text, reply_markup=close_markup(_))
+
+
+# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
