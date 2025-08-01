19564c459fde8620a613cc2b9b4c0cfe4c8b3b63
𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs <s>
2025-08-01 03:17:33 +0000

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs - shuffle.py

--- ShrutiMusic/plugins/admins/shuffle.py
+++ ShrutiMusic/plugins/admins/shuffle.py
@@ -53,3 +53,6 @@ async def admins(Client, message: Message, _, chat_id):
     await message.reply_text(
         _["admin_16"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
+
+
+# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
