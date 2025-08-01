3561c883fb6c0c7d6c071740e7bb6980a7ca4f6f
𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs <s>
2025-08-01 03:17:23 +0000

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs - channel.py

--- ShrutiMusic/plugins/play/channel.py
+++ ShrutiMusic/plugins/play/channel.py
@@ -69,3 +69,6 @@ async def playmode_(client, message: Message, _):
             return await message.reply_text(_["cplay_6"].format(chat.title, cusn))
         await set_cmode(message.chat.id, chat.id)
         return await message.reply_text(_["cplay_3"].format(chat.title, chat.id))
+
+
+# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
