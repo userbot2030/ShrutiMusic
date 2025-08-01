c3905b5789e0c1b2f53f8062e36c510302256396
𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs <s>
2025-08-01 03:17:20 +0000

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs - blchat.py

--- ShrutiMusic/plugins/sudo/blchat.py
+++ ShrutiMusic/plugins/sudo/blchat.py
@@ -81,3 +81,6 @@ async def all_chats(client, message: Message, _):
         await message.reply_text(_["black_8"].format(app.mention))
     else:
         await message.reply_text(text)
+
+
+# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
