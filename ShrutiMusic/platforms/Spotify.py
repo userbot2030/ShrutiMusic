2c6582ee422c1f694c99f450e184ce552ed59025
𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs <s>
2025-08-01 03:36:13 +0000

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/platforms/Spotify.py
+++ ShrutiMusic/platforms/Spotify.py
@@ -121,3 +121,9 @@ class SpotifyAPI:
 
 
 # ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
+
+# ===========================================
+# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
+# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
+# 📢 Telegram Channel : https://t.me/ShrutiBots
+# ===========================================
