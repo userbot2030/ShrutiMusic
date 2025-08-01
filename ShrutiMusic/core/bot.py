03bb8f162a4292fa66d2ce4d8b0e3fb23b201226
𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs <s>
2025-08-01 03:37:55 +0000

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/core/bot.py
+++ ShrutiMusic/core/bot.py
@@ -117,3 +117,9 @@ class Aviax(Client):
 
 
 # ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
+
+# ===========================================
+# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
+# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
+# 📢 Telegram Channel : https://t.me/ShrutiBots
+# ===========================================
