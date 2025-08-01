e7e309efe94b5872f5cef7fe37b4987f161b1c3c
𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs <s>
2025-08-01 03:36:17 +0000

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/plugins/misc/broadcast.py
+++ ShrutiMusic/plugins/misc/broadcast.py
@@ -256,3 +256,9 @@ asyncio.create_task(auto_clean())
 
 
 # ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
+
+# ===========================================
+# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
+# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
+# 📢 Telegram Channel : https://t.me/ShrutiBots
+# ===========================================
