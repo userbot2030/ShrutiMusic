6cc9a95aacd9b04403322f724cca73766ac20c9f
𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs <s>
2025-08-01 03:16:54 +0000

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs - __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -54,3 +54,6 @@ for filename in os.listdir(r"./strings/langs/"):
     except:
         print("There is some issue with the language file inside bot.")
         exit()
+
+
+# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
