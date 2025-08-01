f81ff85b0552dd0a98d2635f5ab2f355a31c453b
𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs <s>
2025-08-01 03:36:11 +0000

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/platforms/Apple.py
+++ ShrutiMusic/platforms/Apple.py
@@ -94,3 +94,9 @@ class AppleAPI:
 
 
 # ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
+
+# ===========================================
+# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
+# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
+# 📢 Telegram Channel : https://t.me/ShrutiBots
+# ===========================================
