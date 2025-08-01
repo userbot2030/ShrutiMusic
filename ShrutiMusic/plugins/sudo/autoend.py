26eb1a4d37bdf4bd2f066488e912b77623370644
𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs <s>
2025-08-01 03:36:26 +0000

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/plugins/sudo/autoend.py
+++ ShrutiMusic/plugins/sudo/autoend.py
@@ -67,3 +67,9 @@ async def auto_leave_chat(_, message: Message):
 
 
 # ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
+
+# ===========================================
+# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
+# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
+# 📢 Telegram Channel : https://t.me/ShrutiBots
+# ===========================================
