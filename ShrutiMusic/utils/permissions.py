fa0eb489eca068bd87bff6e6a3e76f2339769fb7
𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs <s>
2025-08-01 03:37:22 +0000

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/utils/permissions.py
+++ ShrutiMusic/utils/permissions.py
@@ -139,3 +139,9 @@ def adminsOnly(permission):
 
 
 # ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
+
+# ===========================================
+# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
+# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
+# 📢 Telegram Channel : https://t.me/ShrutiBots
+# ===========================================
