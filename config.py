6bdc133b5da6415417520716214edeffee579d52
𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs <s>
2025-08-01 03:35:59 +0000

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- config.py
+++ config.py
@@ -169,3 +169,9 @@ if SUPPORT_GROUP:
 
 
 # ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
+
+# ===========================================
+# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
+# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
+# 📢 Telegram Channel : https://t.me/ShrutiBots
+# ===========================================
