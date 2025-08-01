971064ba961048252552a0549af30e4d39fd5cdb
𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs <s>
2025-08-01 03:36:54 +0000

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/plugins/tools/active.py
+++ ShrutiMusic/plugins/tools/active.py
@@ -99,3 +99,9 @@ async def activevi_(_, message: Message):
 
 
 # ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
+
+# ===========================================
+# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
+# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
+# 📢 Telegram Channel : https://t.me/ShrutiBots
+# ===========================================
