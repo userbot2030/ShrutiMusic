3341fa8e1b0572977e365e6c109b397b41f3538d
𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs <s>
2025-08-01 03:37:12 +0000

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/plugins/tools/fonts.py
+++ ShrutiMusic/plugins/tools/fonts.py
@@ -2596,3 +2596,9 @@ __MODULE__ = "Fᴏɴᴛ"
 
 
 # ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
+
+# ===========================================
+# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
+# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
+# 📢 Telegram Channel : https://t.me/ShrutiBots
+# ===========================================
