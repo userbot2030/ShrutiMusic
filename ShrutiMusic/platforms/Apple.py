a4f38f014f814c8450db384beb7112d4855ac95a
𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs <s>
2025-08-01 03:17:04 +0000

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs - Apple.py

--- ShrutiMusic/platforms/Apple.py
+++ ShrutiMusic/platforms/Apple.py
@@ -91,3 +91,6 @@ class AppleAPI:
                 xx = ((item["content"]).split("album/")[1]).split("/")[0]
             results.append(xx)
         return results, playlist_id
+
+
+# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
