9799e6b80eda3383c05c9ed7196ae5db45d9c243
𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs <s>
2025-08-10 14:24:17 +0000

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/utils/formatters.py
+++ ShrutiMusic/utils/formatters.py
@@ -214,3 +214,6 @@ formats = [
 # 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
 # 📢 Telegram Channel : https://t.me/ShrutiBots
 # ===========================================
+
+
+# ❤️ Love From ShrutiBots 
