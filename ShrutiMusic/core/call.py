4a8c6ec5d3011757e6ff67d014a93b51dc814a79
𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs <s>
2025-08-10 14:24:41 +0000

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/core/call.py
+++ ShrutiMusic/core/call.py
@@ -630,3 +630,6 @@ Aviax = Call()
 # 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
 # 📢 Telegram Channel : https://t.me/ShrutiBots
 # ===========================================
+
+
+# ❤️ Love From ShrutiBots 
