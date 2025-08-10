53dadea76c35dc7194bca3b9fb6c25a9c0baaa23
𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs <s>
2025-08-10 14:22:57 +0000

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/misc.py
+++ ShrutiMusic/misc.py
@@ -104,3 +104,6 @@ def heroku():
 # 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
 # 📢 Telegram Channel : https://t.me/ShrutiBots
 # ===========================================
+
+
+# ❤️ Love From ShrutiBots 
