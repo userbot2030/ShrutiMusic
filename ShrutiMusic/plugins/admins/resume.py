7b9796c3b4a7502a3a886cdba190e43cc3dc38b0
𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs <s>
2025-08-10 14:23:37 +0000

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/plugins/admins/resume.py
+++ ShrutiMusic/plugins/admins/resume.py
@@ -50,3 +50,6 @@ async def resume_com(cli, message: Message, _, chat_id):
 # 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
 # 📢 Telegram Channel : https://t.me/ShrutiBots
 # ===========================================
+
+
+# ❤️ Love From ShrutiBots 
