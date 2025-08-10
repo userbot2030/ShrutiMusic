5173810fe8a7872497a6b9ca48cb5819ef5f0362
𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs <s>
2025-08-10 14:23:32 +0000

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/plugins/admins/pause.py
+++ ShrutiMusic/plugins/admins/pause.py
@@ -50,3 +50,6 @@ async def pause_admin(cli, message: Message, _, chat_id):
 # 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
 # 📢 Telegram Channel : https://t.me/ShrutiBots
 # ===========================================
+
+
+# ❤️ Love From ShrutiBots 
