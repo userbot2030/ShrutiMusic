c011ae420698651dda1b3f051d6721d943842f26
𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs <s>
2025-08-10 14:23:26 +0000

    𝐒ʜʀᴜᴛɪ𝐁ᴏᴛs

--- ShrutiMusic/plugins/bot/help.py
+++ ShrutiMusic/plugins/bot/help.py
@@ -190,3 +190,6 @@ async def helper_cb(client, CallbackQuery, _):
 # 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
 # 📢 Telegram Channel : https://t.me/ShrutiBots
 # ===========================================
+
+
+# ❤️ Love From ShrutiBots 
