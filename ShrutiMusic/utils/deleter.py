2a1d1885e527d0cacda1461023f3f17184d21b99
userbot2030 <166553732+userbot2030@users.noreply.github.com>
2025-08-13 03:49:28 +0700

    deleter.py

--- ShrutiMusic/utils/deleter.py
+++ ShrutiMusic/utils/deleter.py
@@ -1,5 +1,20 @@
 class Deleter:
-    # implementasi class Deleter
+    WHITELIST_USER = {}
+    BLACKLIST_USER = {}
+    SETUP_CHATS = set()
 
-class VerifyAnkes:
-    # implementasi class VerifyAnkes
+    @staticmethod
+    async def setup_antigcast(client, message):
+        # Your setup code here
+        pass
+
+    @staticmethod
+    async def deleter(client, message):
+        # Your delete logic here
+        pass
+
+def VerifyAnkes(func):
+    async def wrapper(*args, **kwargs):
+        # Your verification logic
+        return await func(*args, **kwargs)
+    return wrapper
