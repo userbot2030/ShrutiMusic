181bbdd6d4ae28312a387fdf20d4ff625432f32c
userbot2030 <166553732+userbot2030@users.noreply.github.com>
2025-08-14 00:28:07 +0700

    Delete ShrutiMusic/utils/query_group.py

--- ShrutiMusic/utils/query_group.py
+++
@@ -1,3 +0,0 @@
-# Nilai group handler ini bisa diganti sesuai kebutuhan, biasanya untuk penanda urutan handler Pyrogram.
-# Misal: ankes_group = 20 jika ingin handler ini berjalan di urutan ke-20 pada event loop Pyrogram.
-ankes_group = 20
