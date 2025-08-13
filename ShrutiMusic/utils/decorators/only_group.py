b16d6e08e8ded6dffe14a46f89f72c9fcb7063d2
userbot2030 <166553732+userbot2030@users.noreply.github.com>
2025-08-13 23:39:52 +0700

    only_group.py

--- ShrutiMusic/utils/decorators/only_group.py
+++ ShrutiMusic/utils/decorators/only_group.py
@@ -1,5 +1,12 @@
+from pyrogram import filters
+from functools import wraps
+
+# Decorator: ONLY_GROUP
 def ONLY_GROUP(func):
-    def wrapper(*args, **kwargs):
-        # logika ONLY_GROUP
-        return func(*args, **kwargs)
+    @wraps(func)
+    async def wrapper(client, message, *args, **kwargs):
+        # Pastikan command hanya bisa dipakai di grup
+        if not message.chat.type in ["group", "supergroup"]:
+            return await message.reply("Command ini hanya bisa dipakai di grup.")
+        return await func(client, message, *args, **kwargs)
     return wrapper
