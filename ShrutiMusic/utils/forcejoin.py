52af613a2ad02b2d21d722cdc71e7190cc2b0da9
userbot2030 <166553732+userbot2030@users.noreply.github.com>
2025-08-14 19:40:14 +0700

    forcejoin.py

--- ShrutiMusic/utils/forcejoin.py
+++ ShrutiMusic/utils/forcejoin.py
@@ -3,11 +3,10 @@ from pyrogram.errors import UserNotParticipant
 async def check_force_join(client, message, channel_username):
     try:
         user = await client.get_chat_member(channel_username, message.from_user.id)
-        # Jika user adalah member, lanjut
         if user.status in ["member", "administrator", "creator"]:
             return True
-        return False
     except UserNotParticipant:
         return False
-    except Exception as e:
+    except Exception:
         return False
+    return False
